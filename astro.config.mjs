71d7f576fdf2b52aad749a3f88438c1c738dcfee
Timothy Foster <tf.auroratide@gmail.com>
2022-04-14 20:37:09 -0500

    Point to /tarm as the base since this is hosted on Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://auroratide.github.io',
+    base: '/tarm',
     markdown: {
         syntaxHighlight: 'prism',
     },
