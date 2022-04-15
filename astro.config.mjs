751b72202c7cf65d8910544e6f0cb9cd6c26918e
Timothy Foster <tf.auroratide@gmail.com>
2022-04-15 08:25:01 -0500

    Use base to root everything in tarm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://auroratide.github.io',
-    base: '/tarm',
+    site: 'https://auroratide.github.io/',
+    base: '/tarm/',
     markdown: {
         syntaxHighlight: 'prism',
     },
