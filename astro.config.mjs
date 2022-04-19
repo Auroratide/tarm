a6fd0a910b0859436f94a5109fb0efc0effbdd53
Timothy Foster <tf.auroratide@gmail.com>
2022-04-19 15:12:08 -0500

    Configure base to be empty in dev so styling actually loads

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+
+const dev = process.env.NODE_ENV === 'dev'
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://auroratide.github.io/',
-    base: '/tarm/',
+    base: dev ? '' : '/tarm/',
     markdown: {
         syntaxHighlight: 'prism',
     },
