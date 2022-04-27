07c8baaefef7130abfc6e084b3d115ea21dd547f
Timothy Foster <tf.auroratide@gmail.com>
2022-04-27 14:55:04 -0500

    Apply themes to topics

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+export const themes = {
+    'frontend': 'aurorablue',
+    'software engineering': 'autumnorange',
+    'storytelling': 'fablepink',
+}
+
+export const themeClass = (theme) => `theme-${theme}`
